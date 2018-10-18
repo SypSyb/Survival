742144ae601e72584647f5efae18a7bf1539d036
jeremy dokkum <jeremydokk@hotmail.com>
2018-10-18 12:10:20 +0200

    Made progress with inventory system

---
+++ Assets/scripts/Jeremy/InventoryManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+
+    public GameObject invPanel;
+
+    public ItemDisplay[] itemDisplay;
+    public Item[] items;
+
+    bool buttonClicked;
+    int invSlot;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (invPanel.activeSelf == true)
+            {
+                invPanel.SetActive(false);
+            }
+            else
+            {
+                invPanel.SetActive(true);
+            }
+        }
+
+        InventoryChecker();
+    }
+
+    void InventoryChecker()
+    {
+        int i = 0;
+        if (itemDisplay[i].item == null)
+        {
+            for (i= 0; i <= itemDisplay.Length - 1; i++)
+            {
+                itemDisplay[i].item = items[0];
+            }
+        }
+    }
+}
---
+++ Assets/scripts/Jeremy/Item.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    NONE,
+    FOOD,
+    TOOL,
+    MATERIAL
+}
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class Item : ScriptableObject
+{
+    [Header("Variables for scripting")]
+    public ItemType itemType;
+    public int amount;
+
+    [Header("Variables for the UI")]
+    public new string name;
+    public string desc;
+    public Sprite image;
+    public int itemAmount;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
